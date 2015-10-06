4a616dd6b7164951a21475cab7352af4f673a2d4
athina bitsui <athina.bitsui@west-mec.org>
2015-10-06 13:33:57 -0700

    C.Y.O.A Part 2

---
+++ code-your-own-adventure-part 2.js
@@ -0,0 +1,21 @@
+var age = prompt("what is your age?");
+if (age < 13) {
+    console.log("you may enter, but we not will take responsibiliy");
+} else{
+    console.log("you may enter");
+}
+var userAnswer = prompt("do you want to play?");
+if("yes" == "no") {
+    console.log("will good luck you will needing it.");
+} else {
+    console.log("well to bad you have to.");
+}
+console.log("well back to the story, you story will start at the battle between you and dragon.");
+var slaying = true;
+var youHit = Math.floor(Math.random()*5);
+var damageThisRound = Math.floor(Math.ramdom()*6+2);
+var totalDamage = 2;
+
+while(slaying) {
+
+}
